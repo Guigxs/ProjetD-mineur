59f363a5badd675f882d6ac8ff5cb7a1632da495
Guigxs <guigxs19@gmail.com>
2018-11-10 01:35:35 +0100

    Boutton devient noir

--- D-mineur.py
+++ D-mineur.py
@@ -4,36 +4,40 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.clock import Clock
 
 from kivy.config import Config                         #AUTRES LIBRAIRIES 
-from time import *
+import time
 
 
-class jeu(App): 
+class jeu(App):
     def build (self):
         self.title = "Démineur"                         #titre
         self.icon = "icon.png"
 
-#----------------Boxes----------------------
+#----------------Boxes haut----------------------
         box = BoxLayout(orientation = 'vertical', size_hint=(None, None), size = (400, 500))       #Boxe generale
 
 
         top_box = BoxLayout(orientation='horizontal', size_hint=(1, .1))        #Partie du dessus (top)
 
-
-        self.time = 0 
+        self.sec = 0
         time_box = AnchorLayout(anchor_x='left', anchor_y='top') 
-        time_box.add_widget(Button(text = "Time played : {}".format(self.time), size_hint=(1, 1)))
+        time_box.add_widget(Label(text = "Time played : {}".format(self.sec), size_hint=(1, 1)))
         top_box.add_widget(time_box)                             # Ajout du temps à la sous-boxe
 
+        
         restart_box = AnchorLayout(anchor_x='center', anchor_y='top')
-        restart_box.add_widget(Button(text = "Restart", size_hint=(1, 1)))
+        self.restart_button = Button(text = "Restart", size_hint=(1, 1))
+        self.restart_button.bind(on_release=self._restart)
+        restart_box.add_widget(self.restart_button)
         top_box.add_widget(restart_box)                           # Ajout du boutton restart à la sous-boxe
 
 
-        _points = 0
+        self.points = 0
         points_box = AnchorLayout(anchor_x='right', anchor_y='top')
-        points_box.add_widget(Button(text = "Points : {}".format(_points), size_hint=(1, 1)))
+        points_box.add_widget(Label(text = "Points : {}".format(self.points), size_hint=(1, 1)))
         top_box.add_widget(points_box)                            # Ajout des points à la sous-boxe
 
         box.add_widget(top_box)
@@ -41,25 +45,28 @@ class jeu(App):
 
 #------------------ Grille ----------------------
         ligne = 16                                          #Partie du dessous (bottom)
-        colone = 16
-        bottom_box = GridLayout(rows = ligne, cols = colone)
-        for i in range(0, colone*ligne):
-            self.num = i
-            bottom_box.add_widget(Button())
+        colonne = 16
+        bottom_box = GridLayout(rows = ligne, cols = colonne)
+        for self.i in range(0, colonne*ligne):
+            self.grid_button = Button(text = str(self.i))
+            bottom_box.add_widget(self.grid_button)
+            self.grid_button.bind(on_release=self._grid_affichage)
+
 
         box.add_widget(bottom_box)
-
-
         return box
 
-    def _timing(self):
-        self.time = int(clock())
+
+    def _restart (self, other):                       #opération boutton restart    
+        pass
+
+    def _grid_affichage(self, source):              #affichage des case quand on clique
+        self.grid_button.background_color = (0, 0, 0, 0)
 
 
 
-Config.set('graphics', 'width', '400')              #Configuration graphique
+
+Config.set('graphics', 'width', '400')              #Configuration graphique de la fenètre
 Config.set('graphics', 'height', '500')
-
-
 
 jeu().run()
--- Tests.py
+++ Tests.py
@@ -1,28 +1,65 @@
+import kivy
+
 from kivy.app import App
+
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
 
-from kivy.config import Config
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.base import EventLoop
+from kivy.lang import Builder
 
-EventLoop.window.title = "Coucou"
+import time
 
-class jeu(App):
-    def build (self):
-        
+class CrudeTimerGrid(GridLayout):
+    time = NumericProperty(0)
 
-        layout = AnchorLayout(anchor_x='left', anchor_y='top')
-        btn1 = Button(text='Hello', size_hint = (None, None), size = (100, 400))
-        layout.add_widget(btn1)
-        return layout
+    def tick(self, *_):
+        if self.time > 0:
+            self.time -= 1
+        else:
+            pass
+
+    def start(self, *_):
+        self.cb = Clock.schedule_interval(self.tick,1)
+
+    def pause(self):
+        Clock.unschedule(self.cb)
+
+    # incomplete code
+    def reset(self, *_):
+        pass
 
 
+class CrudeTimerApp(App):
+    def build(self):
+        # Testing timer by initialising timer with 20 seconds
+        return CrudeTimerGrid(time=20)
 
+Builder.load_string('''
+<CrudeTimerGrid>
+    id: timer
+    rows: 2
+    # insert formatting here
 
+    BoxLayout:
+        Label:
+            text: str(timer.time)
 
-jeu().run()
+    BoxLayout:
 
+        Button:
+            text: "Start"
+            on_press: timer.start()
 
+        Button:
+            text: "Pause"
+            on_press: timer.pause()
+
+        Button:
+            text: "Reset"
+            on_press: timer.reset()
+''')
+
+CrudeTimerApp().run()
